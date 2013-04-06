bf4c3e2765f454de822f8819dd9ce442d975d0c6
T. S. Cobb <tscobb@Ts-iMac.local>
2013-04-06 16:21:03 -0400

    added test cases to the project as well as annother build destination

---
+++ GroupQDemo/SpotifySearchTest.h
@@ -0,0 +1,13 @@
+//
+//  SpotifySearchTest.h
+//  GroupQDemo
+//
+//  Created by T. S. Cobb on 4/6/13.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SpotifySearchTest : SenTestCase
+
+@end
---
+++ GroupQDemo/SpotifySearchTest.m
@@ -0,0 +1,13 @@
+//
+//  SpotifySearchTest.m
+//  GroupQDemo
+//
+//  Created by T. S. Cobb on 4/6/13.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import "SpotifySearchTest.h"
+
+@implementation SpotifySearchTest
+
+@end

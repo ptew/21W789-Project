5a66fcfeb5ede2d6ebe9e423742138eed47c6d5e
Jono Matthews <jmm@ks-one-forty-seven.mit.edu>
2013-04-06 17:56:31 -0400

    Stuff for brad to test searching.

---
+++ PipelineStoryboard/PipelineStoryboard/TEMPSEARCHViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEMPSEARCHViewController.h
+//  PipelineStoryboard
+//
+//  Created by Jono Matthews on 4/6/13.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEMPSEARCHViewController : UIViewController
+
+@end

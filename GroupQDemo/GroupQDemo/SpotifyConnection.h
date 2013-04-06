7958e60615a76a43b7a781cc77fbeb5002a8c201
T. S. Cobb <tscobb@Ts-iMac.local>
2013-04-06 17:07:04 -0400

    added unit testing shit

--- GroupQDemo/GroupQDemo/SpotifyConnection.h
+++ GroupQDemo/GroupQDemo/SpotifyConnection.h
@@ -8,13 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import <CocoaLibSpotify.h>
-#import "SpotifyConnectionDelegate.h"
 
 @interface SpotifyConnection : NSObject <SPSessionDelegate>
 
 @property (weak, nonatomic) UIViewController *parent;
 @property (strong, nonatomic) SPPlaybackManager* player;
-@property (strong, nonatomic) id<SpotifyConnectionDelegate> delegate;
 
 - (SpotifyConnection *) initWithParent: (UIViewController *) who;
 

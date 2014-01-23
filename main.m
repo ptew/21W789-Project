68dfe0b7b5b5ce77cabaf0a48359b30c09dc99e9
Parker Tew <ptew@mit.edu>
2014-01-22 23:23:05 -0500

    Fixing up weird git issue.

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  groupq_main.m
+//  GroupQ
+//
+//  Created by T. S. Cobb on 4/6/13.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
